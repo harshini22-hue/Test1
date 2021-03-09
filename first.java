369ab40b3cf6fbacc8ce70161c09c0f98ebf8f11
harshini <harshini.20mis7073@vitap.ac.in>
2021-03-09 23:05:36 +0530

    minor changes made to first.java

--- first.java
+++ first.java
@@ -2,9 +2,8 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
+      int num1 = 5, num2 = 13, num3= 2 sum;
+      sum = num1 + num2 + num3; 
 
       System.out.println("Sum of these numbers: "+sum);
-   }
-}
+ }
